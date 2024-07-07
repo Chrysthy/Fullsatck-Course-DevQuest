0074a2e2d8958c5753d2d247a1b5ebf8db993409
Chrystine Martins <chrystine.martins@outlook.com>
2024-07-07 19:13:54 -0300

    map

--- 4-JavaScript/modulo-javascript-avancado/02-map/index.js
+++ 4-JavaScript/modulo-javascript-avancado/02-map/index.js
@@ -3,3 +3,13 @@ let pessoas = [
     { nome: 'Leon', idade: 4 },
     { nome: 'Collin', idade: 4 }
 ]
+
+
+// let nomeDasPessoas = pessoas.map((pessoa) => `${pessoa.nome} têm ${pessoa.idade} anos de idade.`)
+
+// console.log(nomeDasPessoas);
+
+
+let nomeDasPessoas = pessoas.map(pessoa => pessoa.nome + 'têm' + pessoa.idade + 'anos de idade.')
+
+console.log(nomeDasPessoas);
