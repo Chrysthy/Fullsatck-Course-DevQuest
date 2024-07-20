1037518aacc3776b2af9d94f4dbf9599424b969a
Chrystine Martins <chrystine.martins@outlook.com>
2024-07-20 14:23:41 -0300

    reduce

--- 4-JavaScript/modulo-javascript-avancado/03-reduce/index.js
+++ 4-JavaScript/modulo-javascript-avancado/03-reduce/index.js
@@ -5,4 +5,10 @@ let ordens = [
     { cliente: 'Collin', tipo: 'compra', quantidade: 56, ativo: 'NFLX34' }
 ]
 
-let quantidadeDeOr
+let quantidadeDeOrdens = 0
+
+for (let i = 0; i < ordens.length; i++){
+    quantidadeDeOrdens += ordens[i]. quantidade
+}
+
+console.log(quantidadeDeOrdens);
