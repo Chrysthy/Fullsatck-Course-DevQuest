becf3a2a1be568b5288b9244067f67bfb04e7e0d
Chrystine Martins <chrystine.martins@outlook.com>
2024-07-20 15:14:45 -0300

    Template strings

--- 4-JavaScript/modulo-javascript-avancado/04-interpolacao-de-string/index.js
+++ 4-JavaScript/modulo-javascript-avancado/04-interpolacao-de-string/index.js
@@ -1,6 +1,14 @@
+// const imprimirInformacoesPessoas = (primeiroNome, ultimoNome, idade) => {
+
+//     return "Primeiro Nome: " + primeiroNome + ", último Nome: " + ultimoNome + ". " + "Idade: " + idade
+// }
+
+// console.log(imprimirInformacoesPessoas("Noob", "Saibot", 14));
+
+
 const imprimirInformacoesPessoas = (primeiroNome, ultimoNome, idade) => {
 
-    return "Primeiro Nome " + primeiroNome + " último Nome " + ultimoNome + ". " + "Idade: " + idade
+    return ` Primeiro Nome: ${primeiroNome}, Último Nome: ${ultimoNome}. Idade ${idade}`
 }
 
 console.log(imprimirInformacoesPessoas("Noob", "Saibot", 14));
