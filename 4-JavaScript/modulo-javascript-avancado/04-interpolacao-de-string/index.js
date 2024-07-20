b885b00a5dd7fc54c7b63133a88d4163c8715376
Chrystine Martins <chrystine.martins@outlook.com>
2024-07-20 15:21:52 -0300

    Template strings

--- 4-JavaScript/modulo-javascript-avancado/04-interpolacao-de-string/index.js
+++ 4-JavaScript/modulo-javascript-avancado/04-interpolacao-de-string/index.js
@@ -11,4 +11,12 @@ const imprimirInformacoesPessoas = (primeiroNome, ultimoNome, idade) => {
     return ` Primeiro Nome: ${primeiroNome}, Último Nome: ${ultimoNome}. Idade ${idade}`
 }
 
-console.log(imprimirInformacoesPessoas("Noob", "Saibot", 14));
+// console.log(imprimirInformacoesPessoas("Noob", "Saibot", 14));
+
+
+//chamando uma função dentro do template literal
+
+console.log(`${imprimirInformacoesPessoas("Noob", "Saibot", 14)}, ele é um gato gordo.`);
+
+//Podemos fazer uma so,a
+console.log(`Soma ${10 + 20}`);
