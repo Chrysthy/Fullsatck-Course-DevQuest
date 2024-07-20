ea7a2f1009a753781fc348e8cc3b4728832d7b3f
Chrystine Martins <chrystine.martins@outlook.com>
2024-07-20 14:21:31 -0300

    reduce

---
+++ 4-JavaScript/modulo-javascript-avancado/03-reduce/index.js
@@ -0,0 +1,8 @@
+let ordens = [
+
+    { cliente: 'Noob', tipo: 'compra', quantidade: 56, ativo: 'NFLX34' },
+    { cliente: 'Leon', tipo: 'compra', quantidade: 56, ativo: 'NFLX34' },
+    { cliente: 'Collin', tipo: 'compra', quantidade: 56, ativo: 'NFLX34' }
+]
+
+let quantidadeDeOr
