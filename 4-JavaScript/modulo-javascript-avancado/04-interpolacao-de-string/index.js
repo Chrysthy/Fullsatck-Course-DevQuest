6d20028268a4801747290878811eb68fc0461818
Chrystine Martins <chrystine.martins@outlook.com>
2024-07-20 15:00:31 -0300

    Template strings

---
+++ 4-JavaScript/modulo-javascript-avancado/04-interpolacao-de-string/index.js
@@ -0,0 +1,6 @@
+const imprimirInformacoesPessoas = (primeiroNome, ultimoNome, idade) => {
+
+    return "Primeiro Nome " + primeiroNome + " último Nome " + ultimoNome + ". " + "Idade: " + idade
+}
+
+console.log(imprimirInformacoesPessoas("Noob", "Saibot", 14));
