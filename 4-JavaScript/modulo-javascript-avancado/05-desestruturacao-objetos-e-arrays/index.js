bd7e9cabce844bcf881c578b2b5f252f96a751b9
Chrystine Martins <chrystine.martins@outlook.com>
2024-07-21 20:18:49 -0300

    Desestruturação de objetos e arrays

--- 4-JavaScript/modulo-javascript-avancado/05-desestruturacao-objetos-e-arrays/index.js
+++ 4-JavaScript/modulo-javascript-avancado/05-desestruturacao-objetos-e-arrays/index.js
@@ -7,19 +7,24 @@ let pessoa = { nome: 'Chrystine', sobrenome: 'Vasconcelos', idade: 32 }
 // console.log(nome);
 // console.log(idade);
 
-let { nome: nome, idade: idade, sobrenome: sobrenomeDaPessoa } = pessoa
+let { nome: nome, idade: idade, sobrenome: sobrenomeDaPessoa, sexo = 'feminino' } = pessoa
 
 console.log(nome);
 console.log(idade);
 console.log(sobrenomeDaPessoa);
+console.log(sexo);
 
 
 //Destruturação de arrays
 
 const numeros = [1, 2, 3];
 
-const [um, dois, tres, quatro] = numeros;
+const [um, dois, tres, quatro = 4] = numeros;
 
 console.log(um);
 console.log(dois);
 console.log(tres);
+console.log(quatro);
+
+
+//Se a propriedade ou índice não existir, você pode definir valores padrão colocando o sinal de igual
