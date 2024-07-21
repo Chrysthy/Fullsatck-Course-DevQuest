7c89c3de93f1ff4e6a5f194380d74b51549b9ab6
Chrystine Martins <chrystine.martins@outlook.com>
2024-07-21 19:56:31 -0300

    Desestruturação de objetos e arrays

--- 4-JavaScript/modulo-javascript-avancado/05-desestruturacao-objetos-e-arrays/index.js
+++ 4-JavaScript/modulo-javascript-avancado/05-desestruturacao-objetos-e-arrays/index.js
@@ -1,3 +1,5 @@
+// Destruturação de objetos
+
 let pessoa = { nome: 'Chrystine', sobrenome: 'Vasconcelos', idade: 32 }
 // let nome = pessoa.nome
 // let idade = pessoa.idade
@@ -12,3 +14,12 @@ console.log(idade);
 console.log(sobrenomeDaPessoa);
 
 
+//Destruturação de arrays
+
+const numeros = [1, 2, 3];
+
+const [um, dois, tres, quatro] = numeros;
+
+console.log(um);
+console.log(dois);
+console.log(tres);
