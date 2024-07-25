79f5d8a4bf54fd083fed91af54f05ae63cc22220
Chrystine Martins <chrystine.martins@outlook.com>
2024-07-24 21:11:10 -0300

    Operador Spread

--- 4-JavaScript/modulo-javascript-avancado/06-operador-spread/index.js
+++ 4-JavaScript/modulo-javascript-avancado/06-operador-spread/index.js
@@ -5,8 +5,12 @@ console.log(...pessoas);
 
 //CONCATENAR DOIS ARRAYS
 
-const pessoas1 = ['Noob', 'Leon', 'Collin']
-const pessoas2 = ['Chery', 'Mary', 'James']
-const pessoas3 = ['Joker']
+let pessoas1 = ['Noob', 'Leon', 'Collin']
+let pessoas2 = ['Chery', 'Mary', 'James']
+let pessoas3 = ['Joker']
 
-pessoas1 = pessoas1.concat(pessoas2).concat(pessoas3)
+// pessoas1 = pessoas1.concat(pessoas2).concat(pessoas3)
+
+pessoas1 = [...pessoas1, ...pessoas2, ...pessoas3]
+
+console.log(pessoas1);
