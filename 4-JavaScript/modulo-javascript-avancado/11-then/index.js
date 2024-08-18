c8a2b1501e8b6fa019137671e03d50dd4e978056
Chrystine Martins <chrystine.martins@outlook.com>
2024-08-18 18:31:09 -0300

    add then

---
+++ 4-JavaScript/modulo-javascript-avancado/11-then/index.js
@@ -0,0 +1,47 @@
+/* Possui 4 estados
+
+1. Pendente
+2. Realizada (Resolve)
+3. Recusada (Reject)
+4. Estabelecida
+
+*/
+
+//recebe dois parâmetros: resolve e reject
+// new Promise((resolve, reject) => {
+
+// })
+
+
+
+let ferverAgua = (chaleiraEstaNoFogao, fogaoEstaLigado) => {
+
+    return new Promise((resolve, reject) => {
+
+        if (chaleiraEstaNoFogao && fogaoEstaLigado) {
+
+            console.log("começando o processo de ferver a água");
+
+            resolve();
+
+
+        } else {
+
+            console.log("é necessário ligar o fogão e colocar a chaleira  para fever a água");
+
+            reject();
+
+        };
+
+    });
+};
+
+let chaleiraEstaNoFogao = true;
+let fogaoEstaLigado = true;
+
+let passarCafe = () => console.log('passando café');
+
+
+ferverAgua(chaleiraEstaNoFogao, fogaoEstaLigado).then(passarCafe);
+
+console.log("fazendo café");
