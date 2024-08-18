5348bce01a3f97806aa31fb9d88d86b084b4ab5c
Chrystine Martins <chrystine.martins@outlook.com>
2024-08-18 15:29:30 -0300

    add promise

--- 4-JavaScript/modulo-javascript-avancado/10-promises/index.js
+++ 4-JavaScript/modulo-javascript-avancado/10-promises/index.js
@@ -8,6 +8,36 @@
 */
 
 //recebe dois parâmetros: resolve e reject
-new Promise((resolve, reject) => {
-    
-})
+// new Promise((resolve, reject) => {
+
+// })
+
+
+
+let ferverAgua = (chaleiraEstaNoFogao, fogaoEstaLigado) => {
+
+    return new Promise((resolve, reject) => {
+
+        if (chaleiraEstaNoFogao && fogaoEstaLigado) {
+
+            console.log("começando o processo de ferver a água");
+
+            resolve();
+
+
+        } else {
+
+            console.log("é necessário ligar o fogão e colocar a chaleira  para fever a água");
+
+            reject();
+
+        };
+
+    });
+};
+
+let chaleiraEstaNoFogao = true;
+let fogaoEstaLigado = true;
+
+ferverAgua(chaleiraEstaNoFogao, fogaoEstaLigado);
+console.log("fazendo café");
