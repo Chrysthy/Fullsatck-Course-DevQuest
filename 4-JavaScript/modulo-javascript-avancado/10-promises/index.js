ed03dd15cf559d4ae5c3fb050bc4666a211d9b75
Chrystine Martins <chrystine.martins@outlook.com>
2024-08-18 15:20:12 -0300

    add promise

---
+++ 4-JavaScript/modulo-javascript-avancado/10-promises/index.js
@@ -0,0 +1,13 @@
+/* Possui 4 estados
+
+1. Pendente
+2. Realizada (Resolve)
+3. Recusada (Reject)
+4. Estabelecida
+
+*/
+
+//recebe dois parâmetros: resolve e reject
+new Promise((resolve, reject) => {
+    
+})
