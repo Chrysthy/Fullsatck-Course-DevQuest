55e44bc1ca8a3d50ba2f6cf8c8b904aff2a56d41
Chrystine Martins <chrystine.martins@outlook.com>
2024-09-01 18:45:15 -0300

    add info endpoints

--- 4-JavaScript/modulo-javascript-avancado/index.js
+++ 4-JavaScript/modulo-javascript-avancado/index.js
@@ -1,1 +1,9 @@
 //API
+
+// - Get = buscar alguma informação da api - Ex.: Uma lista de cliente
+
+// - Post = enviar informação para API - Ex.: cadastrar um cliente novo no banco de dados
+
+// - Put = alterar o estado da informação - Ex.: editar informação do cliente no banco de dados
+
+// - Delete = deletar um registro de dados ou de alguma outra plataforma
