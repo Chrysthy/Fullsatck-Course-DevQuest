59d63e8eac6b1c8fbff5441422e271d9cf6a3522
Chrystine Martins <chrystine.martins@outlook.com>
2024-09-01 19:26:43 -0300

    add function tirarUmaCartaDoBaralho

--- 4-JavaScript/modulo-javascript-avancado/16-fetch/index.js
+++ 4-JavaScript/modulo-javascript-avancado/16-fetch/index.js
@@ -1,7 +1,18 @@
 async function criarBaralhoEmbaralhado() {
-    
+
     const url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
 
     const resposta = await fetch(url)
 
+    return await resposta.json()
+
 }
+
+async function tirarUmaCartaDoBaralho() {
+
+    const baralho = await criarBaralhoEmbaralhado()
+
+
+}
+
+tirarUmaCartaDoBaralho()
