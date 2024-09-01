a8e432e835ce9544a2cd1f2af6d8ce94a25ba8ec
Chrystine Martins <chrystine.martins@outlook.com>
2024-09-01 19:19:35 -0300

    add async e await

--- 4-JavaScript/modulo-javascript-avancado/16-fetch/index.js
+++ 4-JavaScript/modulo-javascript-avancado/16-fetch/index.js
@@ -1,7 +1,7 @@
-function criarBaralhoEmbaralhado() {
+async function criarBaralhoEmbaralhado() {
     
     const url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
 
-    
+    const resposta = await fetch(url)
 
 }
