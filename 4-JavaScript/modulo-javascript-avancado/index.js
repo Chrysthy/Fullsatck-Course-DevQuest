6da077f3fd3afd8877c5a24436584d05826a6f43
Chrystine Martins <chrystine.martins@outlook.com>
2024-09-21 15:54:15 -0300

    add loadEvents

--- 4-JavaScript/modulo-javascript-avancado/index.js
+++ 4-JavaScript/modulo-javascript-avancado/index.js
@@ -1,6 +1,8 @@
 const loadUsers = async () => {
+
     return new Promise((resolve, reject) => {
         setTimeout(() => {
+
             resolve([
                 { id: 1, name: 'John' },
                 { id: 2, name: 'Jane' },
@@ -10,8 +12,10 @@ const loadUsers = async () => {
 }
 
 const loadRepositories = async () => {
+
     return new Promise((resolve, reject) => {
         setTimeout(() => {
+
             resolve([
                 { id: 1, name: 'Repository 1' },
                 { id: 2, name: 'Repository 2' },
@@ -19,5 +23,17 @@ const loadRepositories = async () => {
         }, 2000)
     })
 }
+
+const loadEvents = async () => {
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            resolve([
+                { id: 1, name: 'Event 1' },
+                { id: 2, name: 'Event 2' },
+            ])
+        }, 2000)
+    })
 
 }
