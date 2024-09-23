b79f2a902e3773e54d520e616f81aa4eaeccfc90
Chrystine Martins <chrystine.martins@outlook.com>
2024-09-23 20:54:52 -0300

    adicionando ou subtraindo dias, meses e anos

--- 4-JavaScript/modulo-javascript-avancado/18-manipular-datas-js/index.js
+++ 4-JavaScript/modulo-javascript-avancado/18-manipular-datas-js/index.js
@@ -1,3 +1,8 @@
 //Data atual
 const dataAtual = new Date()
 console.log(dataAtual);
+
+//Adicionando dias, meses e anos (pode subtrair também)
+const umDiaDepois = new Date(dataAtual)
+umDiaDepois.setDate(dataAtual.getDate() + 1)
+console.log(umDiaDepois);
