ac96d2d39f3b089fa7e037225ae857ed0d5a78b5
Chrystine Martins <chrystine.martins@outlook.com>
2024-09-28 21:21:51 -0300

    import multiplas funcoes

--- 4-JavaScript/modulo-javascript-avancado/21-modulos-javascript/index.js
+++ 4-JavaScript/modulo-javascript-avancado/21-modulos-javascript/index.js
@@ -1,4 +1,5 @@
-import { somar } from './operacoes-matematicas.js' //pode colocar .js ou não
+import { somar, multiplicar } from './operacoes-matematicas.js' //pode colocar .js ou não
 //importa, o nome da função, de onde e o caminho
 
 console.log(somar(2, 2));
+console.log(multiplicar(3, 2));
