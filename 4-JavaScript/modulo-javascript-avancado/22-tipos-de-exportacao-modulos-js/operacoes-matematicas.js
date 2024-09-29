4d00c85dce805a7cf6c913c863128cb94750af68
Chrystine Martins <chrystine.martins@outlook.com>
2024-09-29 11:20:54 -0300

    update default

--- 4-JavaScript/modulo-javascript-avancado/22-tipos-de-exportacao-modulos-js/operacoes-matematicas.js
+++ 4-JavaScript/modulo-javascript-avancado/22-tipos-de-exportacao-modulos-js/operacoes-matematicas.js
@@ -1,15 +1,18 @@
+//Usando export default
+//pode usar tanto na função quanto depois dela
+
+/*
+
+    export default function somar(numero1, numero2) {
+    return numero1 + numero2
+    }
+
+*/
+
 function somar(numero1, numero2) {
     return numero1 + numero2
 }
 
 export default somar
 
-//Usando export default
-//pode usar tanto na função quanto depois dela
 
-/*
-
-export default function somar(numero1, numero2) {
-    return numero1 + numero2
-}
-*/
