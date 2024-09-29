536d3f633bfccd7a226c074538fe1ff38235bd89
Chrystine Martins <chrystine.martins@outlook.com>
2024-09-29 11:43:59 -0300

    export nomeada

--- 4-JavaScript/modulo-javascript-avancado/22-tipos-de-exportacao-modulos-js/operacoes-matematicas.js
+++ 4-JavaScript/modulo-javascript-avancado/22-tipos-de-exportacao-modulos-js/operacoes-matematicas.js
@@ -16,3 +16,11 @@ function somar(numero1, numero2) {
 export default somar
 
 
+//Usando exportação nomeada
+//se for mais de uma função
+//se for somente uma, irá passar o export na frente do function
+function somar(numero1, numero2) {
+    return numero1 + numero2
+}
+
+export { somar }
