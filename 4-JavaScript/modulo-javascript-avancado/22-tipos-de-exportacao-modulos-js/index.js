9d5ac4a553fdadf441a5c3e86ecc0dd2bfda5ae6
Chrystine Martins <chrystine.martins@outlook.com>
2024-09-29 11:17:05 -0300

    add import somar

---
+++ 4-JavaScript/modulo-javascript-avancado/22-tipos-de-exportacao-modulos-js/index.js
@@ -0,0 +1,7 @@
+//Usando export default
+//não conseguimos usar as chaves no import
+//So podemos ter apenas uma exportação por arquivo
+//a nomenclatura da importação é totalmente independente do nome fa função
+import somar from './operacoes-matematicas.js'
+
+console.log(somar(2,2));
