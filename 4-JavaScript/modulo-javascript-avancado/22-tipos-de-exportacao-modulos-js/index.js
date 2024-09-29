42200c8d02b32e14614edd21a8fc87ea2d5e6153
Chrystine Martins <chrystine.martins@outlook.com>
2024-09-29 11:43:42 -0300

    importando nomeada

--- 4-JavaScript/modulo-javascript-avancado/22-tipos-de-exportacao-modulos-js/index.js
+++ 4-JavaScript/modulo-javascript-avancado/22-tipos-de-exportacao-modulos-js/index.js
@@ -1,7 +1,15 @@
 //Usando export default
 //não conseguimos usar as chaves no import
 //So podemos ter apenas uma exportação por arquivo
-//a nomenclatura da importação é totalmente independente do nome fa função
+//a nomenclatura da importação é totalmente independente do nome da função
 import somar from './operacoes-matematicas.js'
 
-console.log(somar(2,2));
+console.log(somar(2, 2));
+
+//Usando export nomeada
+//precisa usar as chaves
+//podemos ter várias importações nomeadas por arquivo
+//o nome do módulo importado tem que ser o mesmo do módulo exportado
+import { somar } from './operacoes-matematicas.js'
+
+console.log(somar(2, 2));
