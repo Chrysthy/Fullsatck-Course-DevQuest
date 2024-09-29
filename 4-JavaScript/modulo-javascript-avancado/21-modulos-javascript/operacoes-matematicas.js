865302b7b171b34625ca86657b06a8029943c7f1
Chrystine Martins <chrystine.martins@outlook.com>
2024-09-28 21:21:36 -0300

    export multiplas funcoes

--- 4-JavaScript/modulo-javascript-avancado/21-modulos-javascript/operacoes-matematicas.js
+++ 4-JavaScript/modulo-javascript-avancado/21-modulos-javascript/operacoes-matematicas.js
@@ -1,7 +1,11 @@
-export function somar(numero1, numero2) {
+function somar(numero1, numero2) {
     return numero1 + numero2;
 }
 
 function multiplicar(numero1, numero2) {
     return numero1 * numero2;
 }
+
+//Para exportar duas funções dessa maneira, é necessário tirar o export da função e abri um export para passar as funções nela.
+
+export { somar, multiplicar }
