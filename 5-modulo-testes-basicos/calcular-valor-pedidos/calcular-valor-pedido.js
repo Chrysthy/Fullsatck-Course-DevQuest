56712f8fd465640817d3c08ad91368f5a0e566fe
Chrystine Martins <chrystine.martins@outlook.com>
2024-11-17 10:22:06 -0300

    add new item

--- 5-modulo-testes-basicos/calcular-valor-pedidos/calcular-valor-pedido.js
+++ 5-modulo-testes-basicos/calcular-valor-pedidos/calcular-valor-pedido.js
@@ -2,10 +2,13 @@ const meuPedido = {
 
     itens: [
         { nome: 'Poção de vida', valor: 100 },
-        { nome: 'Espada de prata', valor: 1000 }
+        { nome: 'Espada de prata', valor: 1000 },
+        { nome: 'Entrega', valor: 40, entrega: true }
     ]
 
 }
 
 const calcularValorPedido = pedido =>
     pedido.itens.reduce((totalPedidos, pedidoAtual) => totalPedidos + pedidoAtual.valor, 0)
+
+console.log(calcularValorPedido(meuPedido));
