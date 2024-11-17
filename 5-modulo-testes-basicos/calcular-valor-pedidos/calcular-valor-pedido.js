7162896e268479e974d526e881854f0df3ae1e0b
Chrystine Martins <chrystine.martins@outlook.com>
2024-11-17 10:28:26 -0300

    add if else para entrega

--- 5-modulo-testes-basicos/calcular-valor-pedidos/calcular-valor-pedido.js
+++ 5-modulo-testes-basicos/calcular-valor-pedidos/calcular-valor-pedido.js
@@ -11,13 +11,20 @@ const meuPedido = {
 const calcularValorPedido = pedido => {
 
     const valorProdutos = pedido.itens
-        
+
         .filter(item => !item.entrega)
         .reduce((totalPedidos, pedidoAtual) => totalPedidos + pedidoAtual.valor, 0)
 
     // pedido.itens.reduce((totalPedidos, pedidoAtual) => totalPedidos + pedidoAtual.valor, 0)
-    
-   
+
+    const entrega = pedido.itens.filter(item => item.entrega);
+
+    if (valorProdutos > 500) {
+        return valorProdutos;
+
+    } else {
+        return valorProdutos + entrega.valor;
+    }
 
 
 }
