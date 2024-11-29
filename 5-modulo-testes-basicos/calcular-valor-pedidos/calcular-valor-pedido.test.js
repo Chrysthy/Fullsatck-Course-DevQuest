fc363e068707a38c3e5cea3ddb757cca98c8152d
Chrystine Martins <chrystine.martins@outlook.com>
2024-11-28 21:40:35 -0300

    add meu pedido function to test

--- 5-modulo-testes-basicos/calcular-valor-pedidos/calcular-valor-pedido.test.js
+++ 5-modulo-testes-basicos/calcular-valor-pedidos/calcular-valor-pedido.test.js
@@ -1,1 +1,16 @@
 const calcularValorPedido = require('./calcular-valor-pedido.js')
+
+it('não deve cobrar valor de frete quando o valor dos produtos for superior a 500', () => {
+
+    const meuPedido = {
+
+        itens: [
+            { nome: 'Poção de vida', valor: 100 },
+            { nome: 'Espada de prata', valor: 200 },
+            { nome: 'Entrega', valor: 40, entrega: true }
+        ]
+    
+    }
+   
+    calcularValorPedido()
+})
