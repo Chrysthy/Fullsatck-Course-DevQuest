257dcde7ef63c623709f416f0fc45d482e0c366d
Chrystine Martins <chrystine.martins@outlook.com>
2024-11-28 21:59:58 -0300

    add test <= pedido

--- 5-modulo-testes-basicos/calcular-valor-pedidos/calcular-valor-pedido.test.js
+++ 5-modulo-testes-basicos/calcular-valor-pedidos/calcular-valor-pedido.test.js
@@ -19,4 +19,22 @@ it('não deve cobrar valor de frete quando o valor dos produtos for superior a 5
 
     // ASSERT - AFIRMAR (ASSERÇÃO) - QUAL O RESULTADO ESPERADO
     expect(resultado).toBe(2000)
+});
+
+it('deve cobrar valor de frete quando o valor dos produtos for menor a 500', () => { 
+
+    const meuPedido = {
+
+        itens: [
+            { nome: 'Sanduíche', valor: 50 },
+            { nome: 'Entrega', valor: 100, entrega: true }
+        ]
+
+    };
+
+    const resultado = calcularValorPedido(meuPedido);
+
+    expect(resultado).toBe(150);
+
+
 })
