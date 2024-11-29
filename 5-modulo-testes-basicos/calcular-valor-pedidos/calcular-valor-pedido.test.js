a83c863076fdb0e80c417e879a9ee5a4bc5d5dff
Chrystine Martins <chrystine.martins@outlook.com>
2024-11-28 21:31:46 -0300

    exportar function

--- 5-modulo-testes-basicos/calcular-valor-pedidos/calcular-valor-pedido.test.js
+++ 5-modulo-testes-basicos/calcular-valor-pedidos/calcular-valor-pedido.test.js
@@ -1,9 +1,1 @@
-// test('teste funcionou', () => {
-//     expect(true).toBe(true)
-// })
-
-it('should test', () => {
-
-    //assertion ou asserção
-    expect(true).toBe(true)
-})
+const calcularValorPedido = require('./calcular-valor-pedido.js')
