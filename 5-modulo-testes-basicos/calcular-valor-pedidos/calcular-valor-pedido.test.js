2e47889c55a06426672d4292b81b61ca6ebf44e0
Chrystine Martins <chrystine.martins@outlook.com>
2024-11-28 21:56:04 -0300

    add AAA explanation

--- 5-modulo-testes-basicos/calcular-valor-pedidos/calcular-valor-pedido.test.js
+++ 5-modulo-testes-basicos/calcular-valor-pedidos/calcular-valor-pedido.test.js
@@ -2,16 +2,21 @@ const calcularValorPedido = require('./calcular-valor-pedido.js')
 
 it('não deve cobrar valor de frete quando o valor dos produtos for superior a 500', () => {
 
+    // AAA - 3 PASSOS DE CRIAÇÃO DE UM TESTE
+
+    // ARRANGE - ARRUMAR - O OBJETO DE TESTE
     const meuPedido = {
 
         itens: [
             { nome: 'Arco encantado', valor: 2000 },
             { nome: 'Entrega', valor: 40, entrega: true }
         ]
-    
+
     }
-   
+
+    // ACT - AGIR (AÇÃO) - O QUE VAI SER TESTADO
     const resultado = calcularValorPedido(meuPedido)
 
+    // ASSERT - AFIRMAR (ASSERÇÃO) - QUAL O RESULTADO ESPERADO
     expect(resultado).toBe(2000)
 })
