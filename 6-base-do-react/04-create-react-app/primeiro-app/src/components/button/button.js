f5e80271b66d32f1d98fa7ef38bd8d6283c9aa2c
Chrystine Martins <chrystine.martins@outlook.com>
2024-12-01 15:35:09 -0300

    add copments to use styles

--- 6-base-do-react/04-create-react-app/primeiro-app/src/components/button/button.js
+++ 6-base-do-react/04-create-react-app/primeiro-app/src/components/button/button.js
@@ -6,3 +6,7 @@ const Button = () => {
 }
 
 export default Button;
+
+//Para usar o css no componente, você deve importar o css no arquivo do componente
+//Não precisa exportar o css para usar no componente
+//Preciso adicionar className no botão para usar o css
