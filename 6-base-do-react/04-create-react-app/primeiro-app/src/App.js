ad6b8c664e53e5388940e2bc178de9d8fe28bede
Chrystine Martins <chrystine.martins@outlook.com>
2024-12-01 15:17:53 -0300

    import Button

--- 6-base-do-react/04-create-react-app/primeiro-app/src/App.js
+++ 6-base-do-react/04-create-react-app/primeiro-app/src/App.js
@@ -1,39 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/button/button';
 
 function App() {
 
-  //O return sempre deve retornar um elemento, por isso, se você quiser retornar mais de um elemento, você deve envolvê-los em uma div
+  
+  return <Button/>
 
-  //Se tiver só um elemento, não precisa envolver em uma div e nem usar parenteses
-
-  //ex.: return <h1>Olá Mundo</h1>
-
-  //Se você não quiser envolver em uma div, você pode usar o fragment
-
-  //Para não deixar uma div englobado os elementos, podemos usar o fragment. Ele não renderiza nada, mas que serve para envolver outros elementos
-
-  // < > </> é a forma de usar o fragment ou
-  // <React.Fragment> </React.Fragment>
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
 }
 
 export default App;
+
+//O return sempre deve retornar um elemento, por isso, se você quiser retornar mais de um elemento, você deve envolvê-los em uma div
+
+//Se tiver só um elemento, não precisa envolver em uma div e nem usar parenteses
+
+//ex.: return <h1>Olá Mundo</h1>
+
+//Se você não quiser envolver em uma div, você pode usar o fragment
+
+//Para não deixar uma div englobado os elementos, podemos usar o fragment. Ele não renderiza nada, mas que serve para envolver outros elementos
+
+// < > </> é a forma de usar o fragment ou
+// <React.Fragment> </React.Fragment>
