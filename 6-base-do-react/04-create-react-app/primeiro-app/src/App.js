15c029c2036befa9bf3c91502bac5305af8ad221
Chrystine Martins <chrystine.martins@outlook.com>
2024-12-01 15:01:12 -0300

    add return comments

--- 6-base-do-react/04-create-react-app/primeiro-app/src/App.js
+++ 6-base-do-react/04-create-react-app/primeiro-app/src/App.js
@@ -2,6 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  //O return sempre deve retornar um elemento, por isso, se você quiser retornar mais de um elemento, você deve envolvê-los em uma div
+
+  //Se tiver só um elemento, não precisa envolver em uma div e nem usar parenteses
+
+  //ex.: return <h1>Olá Mundo</h1>
+
+  //Se você não quiser envolver em uma div, você pode usar o fragment
+
+  //Para não deixar uma div englobado os elementos, podemos usar o fragment. Ele não renderiza nada, mas que serve para envolver outros elementos
+
+  // < > </> é a forma de usar o fragment
+
   return (
     <div className="App">
       <header className="App-header">
