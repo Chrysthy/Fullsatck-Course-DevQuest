33f0dab47d2b8e22e4b0708fb3abd3bd584c2b2a
Chrystine Martins <chrystine.martins@outlook.com>
2024-11-30 21:37:54 -0300

    add componente de classe

--- 6-base-do-react/01-componentes-de-funcao-e-class/index.js
+++ 6-base-do-react/01-componentes-de-funcao-e-class/index.js
@@ -3,4 +3,9 @@ function Welcome(props) {
     return <h1>Olá, {props.name}</h1>;
 }
   
-
+//Componentes de Classe
+class Welcome extends React.Component {
+    render() {
+      return <h1>Olá, {this.props.name}</h1>;
+    }
+  }
