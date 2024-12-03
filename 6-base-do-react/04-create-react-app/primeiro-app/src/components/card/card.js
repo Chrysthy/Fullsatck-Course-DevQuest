3867e8c18d11749013a7856ee78938adc61c469e
Chrystine Martins <chrystine.martins@outlook.com>
2024-12-02 21:56:57 -0300

    add comments

--- 6-base-do-react/04-create-react-app/primeiro-app/src/components/card/card.js
+++ 6-base-do-react/04-create-react-app/primeiro-app/src/components/card/card.js
@@ -19,3 +19,11 @@ class Card extends React.Component {
 }
 
 export default Card;
+
+//Para criar uma classe, usamos a palavra class seguido da palavra extends, que significa extender alguns caracteristicas de uma outra classe para dentro do nosso componente. Nesse caso estamos estendendo as caracteristicas do React.Component
+
+//Quem está do lado direito está herdando as caracteristicas e do lado esquerdo é quem está passando as heranças
+
+//Precisa importar o React para usar o React.Component
+
+//Dentro da classe, criamos uma função render, que é obrigatório em um componente de classe. E dentro da função render, usando o return para retornamos o que queremos que seja renderizado na tela
