cb2c5ab2a22fc194402e750154aa4cfdd77a6181
Chrystine Martins <chrystine.martins@outlook.com>
2024-12-08 19:48:15 -0300

    add new content in the app.jsx

--- 6-base-do-react/05-prop-types/react-prop-types/src/App.jsx
+++ 6-base-do-react/05-prop-types/react-prop-types/src/App.jsx
@@ -2,25 +2,44 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Count from './components/Count'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const incrementCount = () => {
+    setCount(count + 1)
+  }
+
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       
+      <div>
+
+        <a href="https://vite.dev" target="_blank">
+          
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+
+        </a>
+
+        <a href="https://react.dev" target="_blank">
+          
+          <img src={reactLogo} className="logo react" alt="React logo" />
+
+        </a>
+
+      </div>
+
+      <h1>Vite + React</h1>
+
+      <Count count={count} incrementCount={incrementCount}></Count>
+
       <p className="read-the-docs">
+
         Click on the Vite and React logos to learn more
+
       </p>
+      
     </>
   )
 }
