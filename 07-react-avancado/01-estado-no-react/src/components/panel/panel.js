433af410b6bf31892fecf68cef357f67ccb3e5cc
Chrystine Martins <chrystine.martins@outlook.com>
2025-01-10 16:00:13 -0300

    update content

--- 07-react-avancado/01-estado-no-react/src/components/panel/panel.js
+++ 07-react-avancado/01-estado-no-react/src/components/panel/panel.js
@@ -1,24 +1,33 @@
 import { Component } from 'react'
 import './panel.css'
 
-class Panel extends Component{
-    constructor(){
+class Panel extends Component {
+
+    constructor() {
+
         super()
         this.state = {
             title: 'Título do painel'
         }
     }
 
-    render(){        
-        return(
-            <section className="panel" onClick={
-               () => {              
-                    this.setState(
-                        {title: 'Título novo'}
-                    )
-                }
-            }>
+    render() {
+
+        return (
+
+            <section className="panel"
+                onClick={
+
+                    () => {
+
+                        this.setState(
+                            { title: 'Título novo' }
+                        )
+                    }
+                }>
+
                 <h2>{this.state.title}</h2>
+
             </section>
         )
     }
