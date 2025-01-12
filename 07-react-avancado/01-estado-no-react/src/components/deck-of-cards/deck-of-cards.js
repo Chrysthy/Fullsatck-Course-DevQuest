04592c1292714466cbee466146613fad1bfe793c
Chrystine Martins <chrystine.martins@outlook.com>
2025-01-12 19:48:01 -0300

    add new function and componentDidMount

--- 07-react-avancado/01-estado-no-react/src/components/deck-of-cards/deck-of-cards.js
+++ 07-react-avancado/01-estado-no-react/src/components/deck-of-cards/deck-of-cards.js
@@ -3,10 +3,17 @@ import { Component } from "react";
 async function createDeck() {
 
     const response = await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
-    
+
     const deck = response.json()
 
-    return deck.deck.id
+    return deck.deck_id
+}
+
+async function getCards(deckId) {
+
+    const response = await fetch(`https://deckofcardsapi.com/api/${deckId}/draw/?count=52`);
+
+    return await response.json()
 }
 
 class DeckOfCards extends Component {
@@ -14,8 +21,20 @@ class DeckOfCards extends Component {
     constructor() {
 
         super();
-        this.state = {}
+        this.state = {
+            cards: []
+        }
 
+    }
+
+    componentDidMount() {
+
+        const deckId = await createDeck();
+        const cards = await getCards(deckId);
+
+        this.setState({
+            cards: cards
+        })
     }
 
     render() {
