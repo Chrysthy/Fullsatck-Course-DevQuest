cadfe886706498e883ba4046c49c2184db600ba1
Chrystine Martins <chrystine.martins@outlook.com>
2025-01-12 19:39:19 -0300

    add example of life cycles

---
+++ 07-react-avancado/01-estado-no-react/src/components/deck-of-cards/deck-of-cards.js
@@ -0,0 +1,25 @@
+import { Component } from "react";
+
+class DeckOfCards extends Component {
+
+    constructor() {
+
+        super();
+        this.state = {}
+
+    }
+
+    render() {
+
+        return (
+
+            <section>
+
+                <ul></ul>
+
+            </section>
+        )
+    }
+}
+
+export default DeckOfCards;
