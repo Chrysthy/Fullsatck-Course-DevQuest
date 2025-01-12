a8b3d454a20364f9c316ae2c99ce5a38265b82d7
Chrystine Martins <chrystine.martins@outlook.com>
2025-01-12 19:41:54 -0300

    add create deck function

--- 07-react-avancado/01-estado-no-react/src/components/deck-of-cards/deck-of-cards.js
+++ 07-react-avancado/01-estado-no-react/src/components/deck-of-cards/deck-of-cards.js
@@ -1,5 +1,14 @@
 import { Component } from "react";
 
+async function createDeck() {
+
+    const response = await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
+    
+    const deck = response.json()
+
+    return deck.deck.id
+}
+
 class DeckOfCards extends Component {
 
     constructor() {
