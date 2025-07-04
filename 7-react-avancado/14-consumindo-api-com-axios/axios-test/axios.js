bb7f2d48989263c505c832ca0b2cc179afbaa0d4
Chrystine Martins <chrystine.martins@outlook.com>
2025-07-03 21:36:03 -0300

    add axios example

--- 7-react-avancado/14-consumindo-api-com-axios/axios-test/axios.js
+++ 7-react-avancado/14-consumindo-api-com-axios/axios-test/axios.js
@@ -1,3 +1,43 @@
+import axios from "axios";
+
+export async function fetchUserDetails(element, username) {
+
+    try {
+
+        const response = await axios.get(`https://api.github.com/users/${username}`);
+
+        //console.log(response);
+
+        const user = response.data
+
+        element.innerHTML = `
+            <img src=${user.avatar_url} alt=${user.name} />
+
+            <h1>${user.name ? user.name : "No name"}</h1>
+
+            <p>${user.bio ? user.bio : "No bio"}</p>
+        
+        `
+
+    } catch (error) {
+        console.error(`Error fetching GitHUb user: ${error.message}`);
+
+        element.innerHtml = `<p> Error loading user data</p>`
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+/*
+Usando fetch
 export async function fetchUserDetails(element, username) {
 
     try {
@@ -10,6 +50,15 @@ export async function fetchUserDetails(element, username) {
 
         const user = await response.json();
 
+        element.innerHTML = `
+            <img src=${user.avatar_url} alt=${user.name} />
+
+            <h1>${user.name ? user.name : "No name"}</h1>
+
+            <p>${user.bio ? user.bio : "No bio"}</p>
+        
+        `
+
     } catch (error) {
         console.error(`Error fetching GitHUb user: ${error.message}`);
 
@@ -17,3 +66,4 @@ export async function fetchUserDetails(element, username) {
 
     }
 }
+*/
