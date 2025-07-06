dc37952ad8ab167cdda41c8550f72a160ff5ccf7
Chrystine Martins <chrystine.martins@outlook.com>
2025-07-06 17:39:16 -0300

    import StatusBar and the tag

--- 8-habilidades-avancadas/01-criando-custom-hook-react/react-custom-hooks/src/App.jsx
+++ 8-habilidades-avancadas/01-criando-custom-hook-react/react-custom-hooks/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import SaveButton from './components/SaveButton/SaveButton.jsx'
+import StatusBar from './components/StatusBar/StatusBar.jsx'
 
 function App() {
 
   return (
     <>
+      <StatusBar />
       <SaveButton />
     </>
   )
