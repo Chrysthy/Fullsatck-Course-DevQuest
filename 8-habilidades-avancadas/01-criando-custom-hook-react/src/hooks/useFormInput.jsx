b651849008d4eb04e6a78e1ff83ce85b09047403
Chrystine Martins <chrystine.martins@outlook.com>
2025-07-12 16:03:32 -0300

    create useFormInput hook

---
+++ 8-habilidades-avancadas/01-criando-custom-hook-react/src/hooks/useFormInput.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export function useFormInput(initialValue) {
+
+    const [value, setValue] = useState(initialValue)
+
+    function handleChange(event) {
+        setValue(event.target.value);
+    }
+
+    const inputProps = {
+        value: value,
+        onChange: handleChange,
+    }
+
+
+
+    return inputProps;
+
+}
+
+export default useFormInput
